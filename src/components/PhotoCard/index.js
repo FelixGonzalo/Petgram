bdaff17034261a2ee06c2abe96815658c85efac0
Gonzalo Castro Cubas <felixgonzalocastro@gmail.com>
2022-02-11 01:08:08 -0500

    ADD hook useNearScreen for lazyload

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,34 +1,16 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
 import { Article, ImgWrapper, Img, Button, ContainerOptions } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
   const key = `like-${id}`
+  const [show, element] = useNearScreen()
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  console.log(liked)
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(isIntersecting)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,24 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useNearScreen () {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(isIntersecting)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
+  return [show, element]
+}
