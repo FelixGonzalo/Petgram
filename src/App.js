71fcefc4112e52845991e57cbac0b6fae13ec4a1
Gonzalo Castro Cubas <felixgonzalocastro@gmail.com>
2022-02-17 23:53:37 -0500

    REFACTOR routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
@@ -8,33 +8,26 @@ import { DetailPet } from './pages/DetailPet'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import Context from './Context'
+import { NotFound } from './pages/NotFound'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
+
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Logo />
         <Routes>
+          <Route path='*' element={<NotFound />} />
           <Route path='/' element={<Home />} />
           <Route path='/pet/:id' element={<Home />} />
           <Route path='/detail/:id' element={<DetailPet />} />
+          <Route path='/favs' element={isAuth ? <Favs /> : <Navigate replace to='/login' />} />
+          <Route path='/user' element={isAuth ? <User /> : <Navigate replace to='/login' />} />
+          <Route path='/login' element={!isAuth ? <NotRegisteredUser /> : <Navigate replace to='/' />} />
         </Routes>
-        <Context.Consumer>
-          {
-            ({ isAuth }) =>
-              isAuth
-                ? <Routes>
-                  <Route path='/favs' element={<Favs />} />
-                  <Route path='/user' element={<User />} />
-                </Routes>
-                : <Routes>
-                  <Route path='/favs' element={<NotRegisteredUser />} />
-                  <Route path='/user' element={<NotRegisteredUser />} />
-                </Routes>
-          }
-        </Context.Consumer>
         <Navbar />
       </BrowserRouter>
     </>
