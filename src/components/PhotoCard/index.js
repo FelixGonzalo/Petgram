371269ecbb4e1fe1d75d38bff8cf63906d9d4e01
Gonzalo Castro Cubas <felixgonzalocastro@gmail.com>
2022-02-21 11:56:15 -0500

    ADD PropTypes example

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,6 +4,7 @@ import { Article, ImgWrapper, Img, ContainerOptions } from './styles'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { useLikePhoto } from '../../hooks/useLikePhoto'
 import { FavButton } from '../FavButton'
+import PropTypes from 'prop-types'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
@@ -33,4 +34,19 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       )}
     </Article>
   )
+}
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function (props, propName, componentName) {
+    const propValue = props[propName]
+    if (propValue === undefined) {
+      return new Error(`${propName} Value must be defined`)
+    }
+    if (propValue < 0) {
+      return new Error(`${propName} Value must be greater than 0`)
+    }
+  }
 }
